312216c700d1a8d4a2cd81ccd965268df9d0f908
Peyman <hassani.peyman627@gmail.com>
2026-07-10 13:33:26 +0330

    Add alt solution for reverse string

--- reverse-string/reverse_string.zig
+++ reverse-string/reverse_string.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 /// Writes a reversed copy of `s` to `buffer`.
 // pub fn reverse(buffer: []u8, s: []const u8) []u8 {
 //     var buf_i: usize = 0;
@@ -21,3 +23,14 @@ pub fn reverse(buffer: []u8, s: []const u8) []u8 {
 
     return buffer[0..buf_i];
 }
+
+// pub fn reverse(buffer: []u8, s: []const u8) []u8 {
+//     var buf_i: usize = 0;
+//     var it = std.mem.reverseIterator(s);
+//     while (it.next()) |c| {
+//         buffer[buf_i] = c;
+//         buf_i += 1;
+//     }
+//
+//     return buffer[0..buf_i];
+// }
