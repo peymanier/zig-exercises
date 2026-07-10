a64f6c69e911d17113d03a6b909112957f769db0
Peyman <hassani.peyman627@gmail.com>
2026-07-10 13:26:03 +0330

    Add reverse string with alt

---
+++ reverse-string/reverse_string.zig
@@ -0,0 +1,23 @@
+/// Writes a reversed copy of `s` to `buffer`.
+// pub fn reverse(buffer: []u8, s: []const u8) []u8 {
+//     var buf_i: usize = 0;
+//     for (0..s.len) |i| {
+//         const rev_i = s.len - 1 - i;
+//         buffer[buf_i] = s[rev_i];
+//         buf_i += 1;
+//     }
+//
+//     return buffer[0..buf_i];
+// }
+
+pub fn reverse(buffer: []u8, s: []const u8) []u8 {
+    var buf_i: usize = 0;
+    var i = s.len;
+    while (i > 0) {
+        i -= 1;
+        buffer[buf_i] = s[i];
+        buf_i += 1;
+    }
+
+    return buffer[0..buf_i];
+}
