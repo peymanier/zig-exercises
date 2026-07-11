de907a2fa8d4eee8d410d6548ec9a68404d804e4
Peyman <hassani.peyman627@gmail.com>
2026-07-11 11:28:36 +0330

    Add grains solution

---
+++ grains/grains.zig
@@ -0,0 +1,17 @@
+const std = @import("std");
+
+pub const ChessboardError = error{IndexOutOfBounds};
+
+pub fn square(index: usize) ChessboardError!u64 {
+    if (index > 64 or index == 0) return ChessboardError.IndexOutOfBounds;
+    return std.math.pow(u64, 2, index - 1);
+}
+
+pub fn total() u64 {
+    var acc: u64 = 0;
+    for (1..65) |i| {
+        acc += square(i) catch unreachable;
+    }
+
+    return acc;
+}
