972d59b3f334d00ddadb8564e77cf357ccbaa9a2
Peyman <hassani.peyman627@gmail.com>
2026-07-14 14:06:46 +0330

    Add bottle song solution

--- bottle-song/bottle_song.zig
+++ bottle-song/bottle_song.zig
@@ -1,22 +1,54 @@
 const std = @import("std");
 
-pub fn recite(buffer: []u8, start_bottles: u32, take_down: u32) ![]const u8 {
+pub fn recite(buffer: []u8, start_count: u32, take_count: u32) ![]const u8 {
+    if (take_count < 1) unreachable;
+
+    var pos: usize = 0;
+    var taken: u32 = 0;
+    var take: u32 = 1;
+    while (take <= take_count) : (take += 1) {
+        if (taken > 0) {
+            @memcpy(buffer[pos..][0..2], "\n\n");
+            pos += 2;
+        }
+
+        const verse = try take_once(buffer[pos..], start_count - taken);
+        pos += verse.len;
+
+        taken += 1;
+    }
+
+    return buffer[0..pos];
+}
+
+fn take_once(buffer: []u8, start_count: u32) ![]const u8 {
     const template: []const u8 =
-        \\{s} green bottles hanging on the wall,
-        \\{s} green bottles hanging on the wall,
-        \\And if {s} green bottle should accidentally fall,
-        \\There'll be {s} green bottle hanging on the wall.
+        \\{[start]s} green {[start_bottle]s} hanging on the wall,
+        \\{[start]s} green {[start_bottle]s} hanging on the wall,
+        \\And if one green bottle should accidentally fall,
+        \\There'll be {[left]s} green {[left_bottle]s} hanging on the wall.
     ;
-    const args = getPoemArgs(start_bottles, take_down);
+    const args = getPoemArgs(start_count);
     return try std.fmt.bufPrint(buffer, template, args);
 }
 
-fn getPoemArgs(start_bottles: u32, take_down: u32) struct { []const u8, []const u8, []const u8, []const u8 } {
+const PoemArgs = struct {
+    start: []const u8,
+    left: []const u8,
+    start_bottle: []const u8,
+    left_bottle: []const u8,
+};
+
+fn getPoemArgs(start_count: u32) PoemArgs {
     var buf: [5]u8 = undefined;
-    const start_string = titleCase(&buf, stringFromNumber(start_bottles));
-    const take_down_string = stringFromNumber(take_down);
-    const hanging_string = stringFromNumber(start_bottles - take_down);
-    return .{ start_string, start_string, take_down_string, hanging_string };
+    const start_string = titleCase(&buf, stringFromNumber(start_count));
+    const left_string = stringFromNumber(start_count - 1);
+    return PoemArgs{
+        .start = start_string,
+        .left = left_string,
+        .start_bottle = if (start_count == 1) "bottle" else "bottles",
+        .left_bottle = if (start_count - 1 == 1) "bottle" else "bottles",
+    };
 }
 
 fn titleCase(buf: []u8, word: []const u8) []const u8 {
@@ -27,6 +59,7 @@ fn titleCase(buf: []u8, word: []const u8) []const u8 {
 
 fn stringFromNumber(number: u32) []const u8 {
     return switch (number) {
+        0 => "no",
         1 => "one",
         2 => "two",
         3 => "three",
