e104af3cd9dc09dcf2b440f47d95604fd7b9a40f
Peyman <hassani.peyman627@gmail.com>
2026-07-17 18:18:22 +0330

    Update micro blog

--- micro-blog/micro_blog.zig
+++ micro-blog/micro_blog.zig
@@ -4,15 +4,13 @@ pub fn truncate(phrase: []const u8) []const u8 {
     const view = std.unicode.Utf8View.init(phrase) catch unreachable;
     var it = view.iterator();
 
-    var buf: [20]u8 = undefined;
     var i: usize = 0;
     var count: usize = 0;
     while (it.nextCodepointSlice()) |char| {
-        @memcpy(buf[i .. i + char.len], char);
         i += char.len;
         count += 1;
         if (count == 5) break;
     }
 
-    return buf[0..i];
+    return phrase[0..i];
 }
