61a28b1488cc1f1cf65bc090b5b62144b2df63a6
Peyman <hassani.peyman627@gmail.com>
2026-07-19 16:55:26 +0330

    Update secret handshake to use a packed struct

--- secret-handshake/secret_handshake.zig
+++ secret-handshake/secret_handshake.zig
@@ -1,6 +1,14 @@
 const std = @import("std");
 const mem = std.mem;
 
+const Bits = packed struct(u5) {
+    wink: bool,
+    double_blink: bool,
+    close_your_eyes: bool,
+    jump: bool,
+    reverse: bool,
+};
+
 pub const Signal = enum {
     wink,
     double_blink,
@@ -13,22 +21,13 @@ pub fn calculateHandshake(allocator: mem.Allocator, number: u5) mem.Allocator.Er
     var result = std.ArrayList(Signal).empty;
     errdefer result.deinit(allocator);
 
-    if (number & 1 == 1) {
-        try result.append(allocator, Signal.wink);
-    }
-    if (number >> 1 & 1 == 1) {
-        try result.append(allocator, Signal.double_blink);
-    }
-    if (number >> 2 & 1 == 1) {
-        try result.append(allocator, Signal.close_your_eyes);
-    }
-    if (number >> 3 & 1 == 1) {
-        try result.append(allocator, Signal.jump);
-    }
+    const bits: Bits = @bitCast(number);
 
-    if (number >> 4 & 1 == 1) {
-        std.mem.reverse(Signal, result.items);
-    }
+    if (bits.wink) try result.append(allocator, Signal.wink);
+    if (bits.double_blink) try result.append(allocator, Signal.double_blink);
+    if (bits.close_your_eyes) try result.append(allocator, Signal.close_your_eyes);
+    if (bits.jump) try result.append(allocator, Signal.jump);
+    if (bits.reverse) std.mem.reverse(Signal, result.items);
 
     return result.toOwnedSlice(allocator);
 }
